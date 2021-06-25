8e7194ecd06bffefb78d7820b497c75efc352e08
EnzoDiazDev <enzodiazdev@gmail.com>
2021-06-25 18:30:15 -0300

    small optimization

--- src/Bottie.ts
+++ src/Bottie.ts
@@ -9,11 +9,13 @@ export default class Bottie {
     readonly client:Client
     protected broker:EventBroker
     readonly properties:BotProperties
+    protected command_match:RegExp
 
     constructor(properties:BotProperties){
         this.client = properties.discord_client || new Client();
         this.properties = properties;
         this.broker = properties.broker || new EventBroker(this.client);
+        this.command_match = new RegExp(`${properties.prefix}\\s*`);
     }
 
     /**
@@ -31,16 +33,16 @@ export default class Bottie {
         const message_channel = this.new_EventChannel("message");
         message_channel.handle = message => {
             if(message.content.startsWith(this.properties.prefix)){
+                const content_lower = message.content.toLowerCase();
+
                 // se retira el prefijo del mensaje
-                const content_wo_prefix = message.content.split(" ")[0] === this.properties.prefix ?
-                    message.content.split(" ").slice(1).join(" ").toLowerCase() :
-                    message.content.split("").slice(this.properties.prefix.length).join("").toLowerCase();
+                const without_prefix = content_lower.replace(this.command_match, "");
 
                 // se toma el comando
-                const command_name = content_wo_prefix.split(" ")[0];
+                const command_name = without_prefix.split(" ")[0];
 
                 // se toman los argumentos
-                const command_arguments = content_wo_prefix.split(" ")
+                const command_arguments = without_prefix.split(" ")
                     .slice(1) // se retira el comando
                     .filter(command_argument => command_argument !== "") // se filtran los argumentos vacios
                     .map(command_argument => command_argument.trim()); // se limpian los espacios al inicio y al final del string
@@ -71,7 +73,6 @@ export default class Bottie {
      * @param args The event arguments.
      */
     public emit<BotEvent extends keyof BotEvents>(event:BotEvent, ...args:BotEvents[BotEvent]):void {
-        //@ts-expect-error - its a delegation.
         this.broker.emit(event, ...args);
     }
 
